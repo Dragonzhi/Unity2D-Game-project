88dfbed7a686bec0c4d93047b4ffa47bf47159cc
Dragonzhi <115857555+Dragonzhi@users.noreply.github.com>
2022-11-19 23:00:26 +0800

    第四次备份
    我还是比较喜欢先给自己备份一个
    刚才不小心整没了，只投了社团那边的github，然后被社长操作了一下，下载回来不好使了，我赶紧给自己备份一个。
    修复了移动的一点不明bug

--- Assets/daima/player_wasd.cs
+++ Assets/daima/player_wasd.cs
@@ -10,67 +10,107 @@ public class player_wasd : MonoBehaviour
 
     public LayerMask ground;//检测地面变量
     public Collider2D coll;//碰撞变量
+    public Collider2D coll1;//碰撞变量1
     public Collider2D discoll;//可以关闭的碰撞    
     public float speed;//速度变量
     public float jumpForce;//跳跃变量
     public Transform CellingCheck;//头顶有没有碰撞？
+    public Transform groundCheck;//地面检测
     public int Weapon_type = 0;//武器类型
     public Text Weapon_type_Num;//武器类型显示
 
+    public bool isGround, isJump;
+    bool jumpPressed;
+    int jumpCount;
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
+        coll1 = GetComponent<Collider2D>();
     }
-
-    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPressed = true;
+        }
+    }
+    
     void FixedUpdate()
     {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
         Movement();
-        ChangeAct();
+        Jump();
+        ChangeAnim();
+        //ChangeAct();
         Crouch();
         
-        //调用这个函数
     }
     //移动函数
     void Movement()
     {
-        Crouch();
         //水平移动判断变量 获取水平移动按键值（-1,0,1）
-        float horizontalMove = Input.GetAxis("Horizontal");
-        //面向
-        float facedircetion = Input.GetAxisRaw("Horizontal");
-        //运动动画判断变量
-        float facedircetion1 = Mathf.Abs(facedircetion);
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
         //角色移动
         if (horizontalMove != 0)
         {
-            //移动
-            rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
-            animator.SetFloat("running", facedircetion1);
-        }
-        if (facedircetion != 0)
-        {
             //朝向改变
-            transform.localScale = new Vector3(-facedircetion, 1, 1);
-
+            transform.localScale = new Vector3(-horizontalMove, 1, 1);
+            //animator.SetFloat("running", -horizontalMove);
         }
+        /*
         //角色跳跃
         if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
             animator.SetBool("jumping", true);
-        }
-
-        
-
-
+        }*/
     }
-    
-    //动画-跳跃上升下落判断
+    //新的跳跃函数！
+    void Jump()
+    {   
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        if(isGround && jumpPressed)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if(isJump && jumpPressed && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+    //动画
+    void ChangeAnim()
+    {
+        animator.SetFloat("running", Mathf.Abs(rb.velocity.x));
+        if (isGround)
+        {
+            animator.SetBool("falling", false);
+        }
+        else if (!isGround && rb.velocity.y > 0)
+        {
+            animator.SetBool("jumping", true);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            animator.SetBool("jumping", false);
+            animator.SetBool("falling", true);
+        }
+    }
+
+    /*
     void ChangeAct()
     {
         animator.SetBool("idleing", false);
@@ -96,6 +136,8 @@ public class player_wasd : MonoBehaviour
             animator.SetBool("idleing", true);
         }
     }
+    */
+
     //下蹲
     void Crouch()
     {
